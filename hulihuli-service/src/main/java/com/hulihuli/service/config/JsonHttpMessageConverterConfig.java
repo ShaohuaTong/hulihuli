bf999a4371ca3be05608ccce40337a97e18147ce
ShaohuaTong <t514321723@gmail.com>
2024-12-03 13:05:09 +0800

    add fastjson and JsonHttpMessageConverterConfig

---
+++ hulihuli-service/src/main/java/com/hulihuli/service/config/JsonHttpMessageConverterConfig.java
@@ -0,0 +1,33 @@
+package com.hulihuli.service.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.HttpMessageConverter;
+
+@Configuration
+public class JsonHttpMessageConverterConfig {
+
+    @Bean
+    @Primary
+    public HttpMessageConverter fastJsonHttpMessageConverter() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.MapSortField,
+                // 禁用循环引用
+                SerializerFeature.DisableCircularReferenceDetect
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return fastConverter;
+    }
+
+}
