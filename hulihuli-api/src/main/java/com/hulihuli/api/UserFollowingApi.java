59f279cd08f5b06c8dad4bb9f5034aae2b17e405
ShaohuaTong <t514321723@gmail.com>
2024-12-05 01:30:35 +0800

    add addUserFollowingGroups and getUserFollowingGroups function

--- hulihuli-api/src/main/java/com/hulihuli/api/UserFollowingApi.java
+++ hulihuli-api/src/main/java/com/hulihuli/api/UserFollowingApi.java
@@ -24,24 +24,39 @@ public class UserFollowingApi {
 
     @PostMapping("/user-followings")
     public JsonResponse<String> addUserFollowing(@RequestBody UserFollowing userFollowing) {
-        Long UserId = userSupport.getCurrentUserId();
-        userFollowing.setUserId(UserId);
+        Long userId = userSupport.getCurrentUserId();
+        userFollowing.setUserId(userId);
         userFollowingService.addUserFollowing(userFollowing);
         return JsonResponse.success();
     }
 
     @GetMapping("/user-followings")
     public JsonResponse<List<FollowingGroup>> getUserFollowings() {
-        Long UserId = userSupport.getCurrentUserId();
-        List<FollowingGroup> followingGroups = userFollowingService.getUserFollowings(UserId);
+        Long userId = userSupport.getCurrentUserId();
+        List<FollowingGroup> followingGroups = userFollowingService.getUserFollowings(userId);
         return new JsonResponse<>(followingGroups);
     }
 
     @GetMapping("/user-fans")
     public JsonResponse<List<UserFollowing>> getUserFans() {
-        Long UserId = userSupport.getCurrentUserId();
-        List<UserFollowing> userFans = userFollowingService.getUserFans(UserId);
+        Long userId = userSupport.getCurrentUserId();
+        List<UserFollowing> userFans = userFollowingService.getUserFans(userId);
         return new JsonResponse<>(userFans);
+    }
+
+    @PostMapping("/user-following-groups")
+    public JsonResponse<Long> addUserFollowingGroups(@RequestBody FollowingGroup followingGroup) {
+        Long userId = userSupport.getCurrentUserId();
+        followingGroup.setUserId(userId);
+        Long groupId = userFollowingService.addUserFollowingGroups(followingGroup);
+        return new JsonResponse<>(groupId);
+    }
+
+    @GetMapping("/user-following-groups")
+    public JsonResponse<List<FollowingGroup>> getUserFollowingGroups() {
+        Long userId = userSupport.getCurrentUserId();
+        List<FollowingGroup> list = userFollowingService.getUserFollowingGroups(userId);
+        return new JsonResponse<>(list);
     }
 
 }
