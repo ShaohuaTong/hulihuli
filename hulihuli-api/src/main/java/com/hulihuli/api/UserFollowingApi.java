42e977ff4a2fd34c32092f766e66e48c128a28c2
ShaohuaTong <t514321723@gmail.com>
2024-12-05 00:14:50 +0800

    add UserFollowingApi

---
+++ hulihuli-api/src/main/java/com/hulihuli/api/UserFollowingApi.java
@@ -0,0 +1,47 @@
+package com.hulihuli.api;
+
+import com.hulihuli.api.support.UserSupport;
+import com.hulihuli.domain.FollowingGroup;
+import com.hulihuli.domain.JsonResponse;
+import com.hulihuli.domain.UserFollowing;
+import com.hulihuli.service.UserFollowingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserFollowingApi {
+
+    @Autowired
+    private UserFollowingService userFollowingService;
+
+    @Autowired
+    private UserSupport userSupport;
+
+    @PostMapping("/user-followings")
+    public JsonResponse<String> addUserFollowing(@RequestBody UserFollowing userFollowing) {
+        Long UserId = userSupport.getCurrentUserId();
+        userFollowing.setUserId(UserId);
+        userFollowingService.addUserFollowing(userFollowing);
+        return JsonResponse.success();
+    }
+
+    @GetMapping("/user-followings")
+    public JsonResponse<List<FollowingGroup>> getUserFollowings() {
+        Long UserId = userSupport.getCurrentUserId();
+        List<FollowingGroup> followingGroups = userFollowingService.getUserFollowings(UserId);
+        return new JsonResponse<>(followingGroups);
+    }
+
+    @GetMapping("/user-fans")
+    public JsonResponse<List<UserFollowing>> getUserFans() {
+        Long UserId = userSupport.getCurrentUserId();
+        List<UserFollowing> userFans = userFollowingService.getUserFans(UserId);
+        return new JsonResponse<>(userFans);
+    }
+
+}
