127963b1031e4b6375cd6681e0822ab1c15be767
ShaohuaTong <t514321723@gmail.com>
2024-12-05 20:39:50 +0800

    add t_user_moments table in sql

--- hulihuli-dao/sql/create_table.sql
+++ hulihuli-dao/sql/create_table.sql
@@ -61,4 +61,15 @@ CREATE TABLE IF NOT EXISTS t_user_following (
                                     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户关注表';
 
-
+-- ----------------------------
+-- t_user_moments 的表结构
+-- ----------------------------
+CREATE TABLE IF NOT EXISTS `t_user_moments` (
+                                  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键id',
+                                  `userId` bigint DEFAULT NULL COMMENT '用户id',
+                                  `type` varchar(5) DEFAULT NULL COMMENT '动态类型：0视频 1直播 2专栏动态',
+                                  `contentId` bigint DEFAULT NULL COMMENT '内容详情id',
+                                  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+                                  `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+                                  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户动态表';
