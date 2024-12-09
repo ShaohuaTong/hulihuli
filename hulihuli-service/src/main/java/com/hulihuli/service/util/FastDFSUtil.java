b009ca1173c5e8803af67ebcfa76f4381510ffe9
ShaohuaTong <t514321723@gmail.com>
2024-12-09 10:37:31 +0800

    add resume from break-point function

--- hulihuli-service/src/main/java/com/hulihuli/service/util/FastDFSUtil.java
+++ hulihuli-service/src/main/java/com/hulihuli/service/util/FastDFSUtil.java
@@ -2,14 +2,19 @@ package com.hulihuli.service.util;
 
 import com.github.tobato.fastdfs.domain.fdfs.MetaData;
 import com.github.tobato.fastdfs.domain.fdfs.StorePath;
+import com.github.tobato.fastdfs.service.AppendFileStorageClient;
 import com.github.tobato.fastdfs.service.FastFileStorageClient;
 import com.hulihuli.exception.ConditionException;
+import io.netty.util.internal.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -18,13 +23,19 @@ public class FastDFSUtil {
     @Autowired
     private FastFileStorageClient fastFileStorageClient;
 
-    public String getFileType(MultipartFile file) {
-        if (file.isEmpty()) {
-            throw new ConditionException("非法文件！");
-        }
-        String fileName = file.getOriginalFilename();
-        return fileName.substring(fileName.lastIndexOf("."));
-    }
+    @Autowired
+    private AppendFileStorageClient appendFileStorageClient;
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
+    private static final String PATH_KEY = "path-key:";
+
+    private static final String UPLOADED_SIZE_KEY = "uploaded-size-key:";
+
+    private static final String UPLOADED_NUM_KEY = "uploaded-num-key:";
+
+    private static final String DEFAULT_GROUP = "group1";
 
     // 上传
     public String uploadCommonFile(MultipartFile file) throws IOException {
@@ -34,9 +45,76 @@ public class FastDFSUtil {
         return storePath.getPath();
     }
 
+    // 上传可以断点续传的文件
+    public String uploadAppenderFile(MultipartFile file) throws IOException {
+        String fileType = this.getFileType(file);
+        StorePath storePath = appendFileStorageClient.uploadAppenderFile(DEFAULT_GROUP, file.getInputStream(), file.getSize(), fileType);
+        return storePath.getPath();
+    }
+
+    public void modifyAppenderFile(MultipartFile file, String filePath, long offset) throws IOException {
+        appendFileStorageClient.modifyFile(DEFAULT_GROUP, filePath, file.getInputStream(), file.getSize(), offset);
+    }
+
+    public String uploadFileBySlices(MultipartFile file, String fileMd5, Integer sliceNum, Integer totalSliceNum) throws Exception {
+        if (file == null || sliceNum == null || totalSliceNum == null) {
+            throw new ConditionException("参数异常！");
+        }
+        String pathKey = PATH_KEY + fileMd5;
+        String uploadedSizeKey = UPLOADED_SIZE_KEY + fileMd5;
+        String uploadedNumKey = UPLOADED_NUM_KEY + fileMd5;
+
+        String uploadedSizeStr =  redisTemplate.opsForValue().get(uploadedSizeKey);
+        Long uploadedSize = 0L;
+        if (!StringUtil.isNullOrEmpty(uploadedSizeStr)) {
+            uploadedSize = Long.parseLong(uploadedSizeStr);
+        }
+        String fileType = this.getFileType(file);
+
+        // 上传的是第一个分片
+        if (sliceNum == 1) {
+            String path = this.uploadAppenderFile(file);
+            if (StringUtil.isNullOrEmpty(path)) {
+                throw new ConditionException("上传失败！");
+            }
+            redisTemplate.opsForValue().set(pathKey, path);
+            redisTemplate.opsForValue().set(uploadedNumKey, "1");
+        } else {
+            String path = redisTemplate.opsForValue().get(pathKey);
+            if (StringUtil.isNullOrEmpty(path)) {
+                throw new ConditionException("上传失败！");
+            }
+            this.modifyAppenderFile(file, path, uploadedSize);
+            redisTemplate.opsForValue().increment(uploadedNumKey);
+        }
+        // 修改历史上传分片文件大小
+        uploadedSize += file.getSize();
+        redisTemplate.opsForValue().set(uploadedSizeKey, String.valueOf(uploadedSize));
+        // 如果所有分片全部上传完毕，则清空redis里相关的key和value
+        String uploadedNumStr = redisTemplate.opsForValue().get(uploadedNumKey);
+        Integer uploadedNum = Integer.valueOf(uploadedNumStr);
+        String resultPath = "";
+        if (uploadedNum.equals(totalSliceNum)) {
+            resultPath = redisTemplate.opsForValue().get(pathKey);
+            List<String> keyList = Arrays.asList(pathKey, uploadedSizeKey, uploadedNumKey);
+            redisTemplate.delete(keyList);
+        }
+        return resultPath;
+    }
+
     //删除
     public void deleteFile(String filePath){
         fastFileStorageClient.deleteFile(filePath);
     }
+
+    public String getFileType(MultipartFile file) {
+        if (file.isEmpty()) {
+            throw new ConditionException("非法文件！");
+        }
+        String fileName = file.getOriginalFilename();
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+
+
 
 }
